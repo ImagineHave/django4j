8fc55c4e51a476c3addc99791b65240706521314
cwih <chris.williams@imagine-have.xyz>
2017-10-18 19:41:29 +0000

    fixed typeError

--- s4j/views.py
+++ s4j/views.py
@@ -26,10 +26,10 @@ class Bible2View(APIView):
         return Response("k", status=status.HTTP_200_OK)
         
     def process(self):
-        bookKey  = open("json/key_english.json").read()
-        genreKey  = open("json/key_genre_english.json").read()
-        passageBible = open("json/testbible1.json").read()
-        processBible = open("json/testbible2.json").read()
+        bookKey  = open("json/key_english.json", "w", encoding="utf8")
+        genreKey  = open("json/key_genre_english.json", "w", encoding="utf8")
+        passageBible = open("json/testbible1.json", "w", encoding="utf8")
+        processBible = open("json/testbible2.json", "w", encoding="utf8")
         
         self.deleteObjects(FieldModel.objects)
         self.deleteObjects(AnswerModel.objects)
