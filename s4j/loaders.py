e4728764f2f0bf817895dd4593af74d4dd806ac3
cwih <chris.williams@imagine-have.xyz>
2017-12-02 22:10:46 +0000

    logging

--- s4j/loaders.py
+++ s4j/loaders.py
@@ -3,6 +3,10 @@ from models import *
 from django.db import connection
 from s4j.tools import *
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
+
 class BibleLoader():
     
     def process(self):
