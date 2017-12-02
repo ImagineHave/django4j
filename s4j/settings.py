c2c3c885cf09596600b2f90e58945e37c94d3cbf
cwih <chris.williams@imagine-have.xyz>
2017-12-02 21:58:28 +0000

    static

--- s4j/settings.py
+++ s4j/settings.py
@@ -144,11 +144,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    BASE_DIR.child('s4j').child('static'),
-)
+  		  
+# Extra places for collectstatic to find static files.		  
+STATICFILES_DIRS = (		  
+BASE_DIR.child('myapp').child('static'),		 
+os.path.join(PROJECT_ROOT, 'static'),
+)		  
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
