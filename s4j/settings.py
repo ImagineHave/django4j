18ba35fa18b4a7b4273df154666500415ba6721d
cwih <chris.williams@imagine-have.xyz>
2017-12-02 21:32:38 +0000

    added in project_dir!

--- s4j/settings.py
+++ s4j/settings.py
@@ -151,6 +151,9 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MESSAGE_LEVEL = message_constants.SUCCESS
